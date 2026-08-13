0ce2123e147ea92f600a75cf71f86e54dcf7a9c7
Tariqul Islam <ts.communicationmagura@gmail.com>
2026-08-13 17:08:57 +0600

    fix: restore safe automatic chunking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,22 +7,6 @@ export default defineConfig({
   build: {
     target: 'es2020',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes('node_modules')) return
-
-          if (/[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/.test(id)) {
-            return 'react'
-          }
-          if (/[\\/]node_modules[\\/](@mui|@emotion)[\\/]/.test(id)) return 'mui'
-          if (/[\\/]node_modules[\\/](@tanstack|axios)[\\/]/.test(id)) return 'query'
-          if (/[\\/]node_modules[\\/]recharts[\\/]/.test(id)) return 'charts'
-          if (/[\\/]node_modules[\\/]react-hook-form[\\/]/.test(id)) return 'forms'
-          if (/[\\/]node_modules[\\/]date-fns[\\/]/.test(id)) return 'dates'
-        },
-      },
-    },
   },
 
   server: {
